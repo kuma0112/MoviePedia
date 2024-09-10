81e71345e00ade863b3f23961acdcb25c6bc3768
kuma0112 <lsj23871@gmail.com>
2024-09-10 20:25:46 +0900

    init commit

---
+++ src/main/java/com/supershy/moviepedia/MoviePediaApplication.java
@@ -0,0 +1,13 @@
+package com.supershy.moviepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supershy/moviepedia/MoviePediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supershy.moviepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
