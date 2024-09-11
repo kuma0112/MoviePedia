ab197c55903ced93e425e81fb0ab2f431d160109
kuma0112 <lsj23871@gmail.com>
2024-09-11 14:50:18 +0900

    feat: Like 엔티티 및 LikeId 클래스 생성

--- src/main/java/com/supershy/moviepedia/like/entity/Like.java
+++ src/main/java/com/supershy/moviepedia/like/entity/Like.java
@@ -1,7 +1,29 @@
 package com.supershy.moviepedia.like.entity;
 
+import com.supershy.moviepedia.like.entity.id.LikeId;
+import com.supershy.moviepedia.member.entity.Member;
+import com.supershy.moviepedia.movie.entity.Movie;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Like {
 
+    @EmbeddedId
+    private LikeId likeId;
 
+    @ManyToOne
+    @MapsId("memberId")
+    @JoinColumn(nullable = false)
+    private Member member;
 
+    @ManyToOne
+    @MapsId("movieId")
+    @JoinColumn(nullable = false)
+    private Movie movie;
 }
