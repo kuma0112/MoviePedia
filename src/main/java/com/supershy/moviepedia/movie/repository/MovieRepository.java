7318108593d5722c793cd98b0d8c615d02ca673c
kuma0112 <lsj23871@gmail.com>
2024-09-11 15:10:47 +0900

    feat: Movie Repository 생성

--- src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
+++ src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
@@ -1,4 +1,7 @@
 package com.supershy.moviepedia.movie.repository;
 
-public interface MovieRepository {
+import com.supershy.moviepedia.movie.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
