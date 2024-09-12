efef95cdd78424919de783f213ae71497684cfb4
kuma0112 <lsj23871@gmail.com>
2024-09-12 09:19:01 +0900

    feat: LikeRepository 생성

--- src/main/java/com/supershy/moviepedia/like/repository/LikeRepository.java
+++ src/main/java/com/supershy/moviepedia/like/repository/LikeRepository.java
@@ -4,4 +4,5 @@ import com.supershy.moviepedia.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByMember_MemberIdAndMovie_MovieId(Long memberId, Long movieId);
 }
