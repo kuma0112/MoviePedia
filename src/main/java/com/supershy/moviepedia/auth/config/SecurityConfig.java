7e3c8bd084a6b55a75ee1e88a78843f4100d1383
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-12 14:44:36 +0900

    feat: SecurityConfig 기본 구조 구현

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.supershy.moviepedia.auth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS // JWT TOken 인증방식으로 세션을 필요 없으므로 비활성화
+                ))
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/members").access()
+//                        .requestMatchers("/api/members/login").access()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin.disable())
+                .logout(logout -> logout.disable());
+
+        return  http.build();
+    }
+
 }
