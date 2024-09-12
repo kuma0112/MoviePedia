3a10db2445a62fa54fc94ae0be24536c8e165cb0
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-12 14:44:36 +0900

    feat: SecurityConfig 기본 구조 구현

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -4,17 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
