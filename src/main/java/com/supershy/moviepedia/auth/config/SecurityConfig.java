0dc0f03495b4ba0bf9b2800995be659c0a862c8f
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-12 14:44:36 +0900

    feat: SecurityConfig 기본 구조 구현

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -1,23 +1,13 @@
 package com.supershy.moviepedia.auth.config;
 
-import com.supershy.moviepedia.auth.filter.JwtAuthenticationFileter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
@@ -28,10 +18,10 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS // JWT TOken 인증방식으로 세션을 필요 없으므로 비활성화
                 ))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/members/" ,"/api/members/login").permitAll()
+//                        .requestMatchers("/api/members").access()
+//                        .requestMatchers("/api/members/login").access()
                         .anyRequest().authenticated()
                 )
-
                 .formLogin(formLogin -> formLogin.disable())
                 .logout(logout -> logout.disable());
 
