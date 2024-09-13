cde8d4b32f483d45df6bbe216e4474c46bab9624
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-13 13:30:00 +0900

    feat: PasssWordEncoder Config에 적용

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -1,19 +1,22 @@
 package com.supershy.moviepedia.auth.config;
 
+import com.supershy.moviepedia.auth.filter.JwtAuthenticationFileter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Bean
@@ -25,10 +28,10 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS // JWT TOken 인증방식으로 세션을 필요 없으므로 비활성화
                 ))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/members").access()
-//                        .requestMatchers("/api/members/login").access()
+                        .requestMatchers("/api/members/" ,"/api/members/login").permitAll()
                         .anyRequest().authenticated()
                 )
+
                 .formLogin(formLogin -> formLogin.disable())
                 .logout(logout -> logout.disable());
 
