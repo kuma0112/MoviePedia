56e150068aea48cd40998a5d036fda726bb72831
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-15 08:12:33 +0900

    feat: JwtTokenProvider 구현

---
+++ src/main/java/com/supershy/moviepedia/auth/utils/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.supershy.moviepedia.auth.utils;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long validityInmilliseconds;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey,
+                            @Value("${jwt.token-validity-in-seconds}") long validityInSeconds) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.validityInmilliseconds = validityInSeconds * 100;
+    }
+
+    public String createToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInmilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            // 로그 처리
+            return false;
+        }
+    }
+}
