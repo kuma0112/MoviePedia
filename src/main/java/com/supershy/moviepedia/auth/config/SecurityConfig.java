1ba86114a8f174953d59f709ac935a6f66eacec1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-15 08:19:56 +0900

    feat: SecurityConfig 구현

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package com.supershy.moviepedia.auth.config;
 
+import com.supershy.moviepedia.auth.filter.JwtAuthenticationFileter;
+import com.supershy.moviepedia.auth.utils.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Bean
@@ -25,10 +30,11 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS // JWT TOken 인증방식으로 세션을 필요 없으므로 비활성화
                 ))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/members").access()
-//                        .requestMatchers("/api/members/login").access()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/api/members").permitAll() // 회원가입 엔드포인트
+                        .requestMatchers("/api/members/login").permitAll() // 로그인 엔드포인트
+                        .anyRequest().authenticated()) // 그 외 모든 요청은 인증 필요
+                .addFilterBefore(new JwtAuthenticationFileter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
                 .formLogin(formLogin -> formLogin.disable())
                 .logout(logout -> logout.disable());
 
