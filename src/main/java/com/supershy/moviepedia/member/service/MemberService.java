7aecb3398a87fb786fb3ba33d1d956c1146753b2
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-15 16:45:17 +0900

    feat: Service 회원가입 기능 구현

--- src/main/java/com/supershy/moviepedia/member/service/MemberService.java
+++ src/main/java/com/supershy/moviepedia/member/service/MemberService.java
@@ -1,4 +1,7 @@
 package com.supershy.moviepedia.member.service;
 
+import com.supershy.moviepedia.member.dto.MemberDto;
+
 public interface MemberService {
+    void registerMember(MemberDto memberDto);
 }
