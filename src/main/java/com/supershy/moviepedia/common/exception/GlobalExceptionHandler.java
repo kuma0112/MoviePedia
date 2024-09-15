207213c0f04552f46928acaa2786108a340ce6a4
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-15 16:45:52 +0900

    feat: Handler 추가

--- src/main/java/com/supershy/moviepedia/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/supershy/moviepedia/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.supershy.moviepedia.common.exception;
 
+import com.supershy.moviepedia.member.dto.MessageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        // 에러 메시지들을 JSON으로 변환해 응답 메시지에 포함
+        return ResponseEntity.badRequest().body(new MessageResponse(errors.toString()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<MessageResponse> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+    }
 }
