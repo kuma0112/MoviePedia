df572c9c6fa666d37a6e81bc09ab856887c4d15c
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-16 01:39:25 +0900

    feat: CustomUserDetails 및 MemberDetailsService 구현

--- src/main/java/com/supershy/moviepedia/auth/service/MemberDetailsService.java
+++ src/main/java/com/supershy/moviepedia/auth/service/MemberDetailsService.java
@@ -1,4 +1,26 @@
 package com.supershy.moviepedia.auth.service;
 
-public class MemberDetailsService {
+import com.supershy.moviepedia.auth.model.CustomUserDetails;
+import com.supershy.moviepedia.member.entity.Member;
+import com.supershy.moviepedia.member.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 가진 사용자를 찾을 수 없습니다: " + email));
+        return new CustomUserDetails(member);
+    }
 }
