7750963b50a5842580886fb688683cce3edd6c54
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-15 13:31:11 +0900

    feat: 회원가입 Dto 구현

--- src/main/java/com/supershy/moviepedia/member/dto/MemberDto.java
+++ src/main/java/com/supershy/moviepedia/member/dto/MemberDto.java
@@ -1,4 +1,10 @@
 package com.supershy.moviepedia.member.dto;
 
+import lombok.Getter;
+
+@Getter
 public class MemberDto {
+    private String email;
+    private String nickname;
+    private String password;
 }
