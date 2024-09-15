3714167507f152fe05fcc54f290bbcd50b7d4a9c
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-16 01:38:47 +0900

    feat: JWT 보안 설정

--- src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
+++ src/main/java/com/supershy/moviepedia/auth/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.supershy.moviepedia.auth.filter.JwtAuthenticationFilter;
 import com.supershy.moviepedia.auth.utils.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +24,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
@@ -30,12 +37,13 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS // JWT TOken 인증방식으로 세션을 필요 없으므로 비활성화
                 ))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/members/").permitAll() // 회원가입 엔드포인트
-                        .requestMatchers("/api/members/login/").permitAll() // 로그인 엔드포인트
+                        .requestMatchers("/api/members/**", "/api/movies/**").permitAll() // 회원가입, 로그인 엔드포인트
+                        .requestMatchers("/", "/pages/**", "/WEB-INF/jsp/**").permitAll() // JSP 페이지에 대한 접근 허용
+                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll() // 정적 리소스 허용
                         .anyRequest().authenticated()) // 그 외 모든 요청은 인증 필요
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
-                .formLogin(formLogin -> formLogin.disable())
+                .formLogin(form -> form.disable())
                 .logout(logout -> logout.disable());
 
         return  http.build();
