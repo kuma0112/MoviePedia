74d601c1d7ab66c1f6e537cbaaab0a54611ae9dd
kuma0112 <lsj23871@gmail.com>
2024-09-18 00:14:22 +0900

    refactor: N+1 해결. fetch join 사용

--- src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
+++ src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
     @Query(value = "SELECT DISTINCT m FROM Movie m LEFT JOIN FETCH m.genre LEFT JOIN FETCH m.reviews", 
            countQuery = "SELECT COUNT(m) FROM Movie m")
     Page<Movie> findAllWithReviewsAndGenre(Pageable pageable);
+
+    @Query(value = "SELECT DISTINCT m FROM Movie m JOIN FETCH m.genre JOIN FETCH m.reviews r JOIN FETCH r.member WHERE m.movieId = :movieId")
+    Optional<Movie> findById(Long movieId);
 }
