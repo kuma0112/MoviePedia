06d7bdbbe5f9a837784eedd0ac6fca18c4b4acc4
kuma0112 <lsj23871@gmail.com>
2024-09-19 15:21:36 +0900

    refactor: serializable implement

--- src/main/java/com/supershy/moviepedia/movie/dto/MovieListDto.java
+++ src/main/java/com/supershy/moviepedia/movie/dto/MovieListDto.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
-public class MovieListDto {
+public class MovieListDto implements Serializable {
     private List<MovieDto> movieList;
     private int totalElements;
 }
