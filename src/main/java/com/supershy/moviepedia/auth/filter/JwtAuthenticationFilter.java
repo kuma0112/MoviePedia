bd9c93bdaf51a818284add87624e6d76fe29a40c
kuma0112 <lsj23871@gmail.com>
2024-09-19 09:27:54 +0900

    refactor: 시큐리티 수정 Principal에 email -> userDetails 객체 담기도록

--- src/main/java/com/supershy/moviepedia/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/supershy/moviepedia/auth/filter/JwtAuthenticationFilter.java
@@ -6,20 +6,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
+@RequiredArgsConstructor
 // OncePerRequestFilter를 상속받아 모든 요청에 대해 한 번씩 필터가 적용되도록 합니다.
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private JwtTokenProvider jwtTokenProvider;
-
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService detailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -28,9 +29,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if(token != null && jwtTokenProvider.validateToken(token)){
             String email = jwtTokenProvider.getEmailFromToken(token);
+            UserDetails userDetails = detailsService.loadUserByUsername(email);
+
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(
-                            email, // principal (사용자 식별자)
+                            userDetails, // principal (사용자 식별자)
                             null,  // credentials (자격 증명, JWT에서는 불필요하므로 null)
                             Collections.emptyList() // authorities (권한 목록)
                     );
