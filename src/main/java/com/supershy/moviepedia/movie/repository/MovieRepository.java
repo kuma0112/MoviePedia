7b5290225ee42c9eadfa48baf4e0ed88597fa17a
jiyun829 <rlacl829@pusan.ac.kr>
2024-09-19 13:27:40 +0900

    modify: upComing api 추가

--- src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
+++ src/main/java/com/supershy/moviepedia/movie/repository/MovieRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param; 
 
 import java.util.Optional;
 
@@ -14,10 +15,11 @@ public interface MovieRepository extends CrudRepository<Movie, Long> {
            countQuery = "SELECT COUNT(m) FROM Movie m")
     Page<Movie> findAllWithReviewsAndGenre(Pageable pageable);
 
-    @Query(value = "SELECT DISTINCT m FROM Movie m JOIN FETCH m.genre JOIN FETCH m.reviews r JOIN FETCH r.member WHERE m.movieId = :movieId")
-    Optional<Movie> findById(Long movieId);
+    @Query(value = "SELECT DISTINCT m FROM Movie m LEFT JOIN FETCH m.genre LEFT JOIN FETCH m.reviews r LEFT JOIN FETCH r.member WHERE m.movieId = :movieId")
+    Optional<Movie> findById(@Param("movieId") Long movieId);  
+    
     
     @Query(value = "SELECT DISTINCT m FROM Movie m LEFT JOIN FETCH m.genre LEFT JOIN FETCH m.reviews r LEFT JOIN FETCH r.member WHERE m.releaseState = 'UPCOMING'",
-    	       countQuery = "SELECT COUNT(m) FROM Movie m WHERE m.releaseState = 'UPCOMING'")
-    	Page<Movie> findUpcomingMovies(Pageable pageable);
+           countQuery = "SELECT COUNT(m) FROM Movie m WHERE m.releaseState = 'UPCOMING'")
+    Page<Movie> findUpcomingMovies(Pageable pageable);
 }
