a726b92b6e6ca05d578088464f19d63cd261b9b8
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-21 15:19:27 +0900

    refactor: Exception 분리

---
+++ src/main/java/com/supershy/moviepedia/common/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.supershy.moviepedia.common.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
