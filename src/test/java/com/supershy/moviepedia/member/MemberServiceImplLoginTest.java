b2f80bbb8e36763f39a59a8b18d45e0aa4c1123f
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-24 11:39:21 +0900

    feat: 로그인 테스트 코드

---
+++ src/test/java/com/supershy/moviepedia/member/MemberServiceImplLoginTest.java
@@ -0,0 +1,10 @@
+package com.supershy.moviepedia.member;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceImplLoginTest {
+
+
+}
