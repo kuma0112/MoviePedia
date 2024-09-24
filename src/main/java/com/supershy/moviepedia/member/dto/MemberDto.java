248fedc4fece74a85b1b505dc58e9c18f4987a9e
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-24 17:09:36 +0900

    refactor: Constructor 어노테이션 추가

--- src/main/java/com/supershy/moviepedia/member/dto/MemberDto.java
+++ src/main/java/com/supershy/moviepedia/member/dto/MemberDto.java
@@ -3,14 +3,12 @@ package com.supershy.moviepedia.member.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import jakarta.validation.constraints.Pattern;
-import lombok.ToString;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class MemberDto {
