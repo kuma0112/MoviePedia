643d66e36470079cc2c8cbae1ba5f159dd6a00fc
kuma0112 <lsj23871@gmail.com>
2024-09-30 11:39:19 +0900

    feat: 리뷰 수정 삭제를 위한 dto 값 추가

--- src/main/java/com/supershy/moviepedia/member/dto/MyPageReviewDto.java
+++ src/main/java/com/supershy/moviepedia/member/dto/MyPageReviewDto.java
@@ -9,13 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MyPageReviewDto {
+    private Long reviewId;
     private String movieTitle;
+    private Long movieId;
     private String content;
     private String nickname;
 
     public static MyPageReviewDto fromEntity(Review review) {
         return new MyPageReviewDto(
+                review.getReviewId(),
                 review.getMovie().getTitle(),
+                review.getMovie().getMovieId(),
                 review.getContent(),
                 review.getMember().getNickname());
     }
